61d7e9cc532a09bc99f72360ac80550c4517a5ad
Michael Bleigh <mbleigh@mbleigh.com>
2013-10-20 02:27:55 +0000

    Try to redirect bare domain to www.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Userpub::Application.routes.draw do
+  constraints host: ENV['APP_HOST'] do
+    match '*path', redirect("http://www.#{ENV['APP_HOST']}/%{path}")
+  end
+  
   constraints LanderConstraint.new do
     root to: 'lander#show', id: 'home'
   end
